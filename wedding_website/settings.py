aa5174aeeb61b4f186a13deb66a7b74ad4295739
Harry L. <harry@spolem.net>
2025-11-06 15:53:42 -0600

    Updating file structure and db attachment
    After a good night's rest, I can get back with a clear mind lol. Simple fixes coming.

--- wedding_website/settings.py
+++ wedding_website/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import environ
 import os
-import dj_database_url
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -16,18 +15,6 @@ SECRET_KEY = env("SECRET_KEY", default="change_me")
 DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-@jv0c9^lg*w0)v34)k9@xf=2qt^wr%*hn=b5@w!)89y%a3i03-'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,26 +60,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wedding_website.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT', default='5432'),
-    }
+    "default": env.db(),
 }
 
 LOGGING = {
@@ -133,10 +102,6 @@ TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = env.str("STATIC_URL", default="/static/")
 STATIC_ROOT = env.str("STATIC_ROOT", default=BASE_DIR / "staticfiles")
