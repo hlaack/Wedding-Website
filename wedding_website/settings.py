52e267dcd17b1c9c820ad669fcc95d327e8848d8
Harry L. <harry@spolem.net>
2025-11-06 02:30:28 -0600

    Trying a different format.

--- wedding_website/settings.py
+++ wedding_website/settings.py
@@ -85,7 +85,14 @@ WSGI_APPLICATION = 'wedding_website.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.config(default=env('DATABASE_URL')),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT', default='5432'),
+    }
 }
 
 LOGGING = {
