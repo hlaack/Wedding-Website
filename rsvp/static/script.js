6001be445fe32d488275406a5ed602a6ab061fdf
Harry L. <harry@spolem.net>
2026-01-21 17:39:56 -0600

    Add page transitions, photo loader
    Added page transition animations and made it so the photo grid does not display until the DOM Content is loaded. Also made a few adjustments to the Q&A page.

--- rsvp/static/script.js
+++ rsvp/static/script.js
@@ -1,3 +1,5 @@
+document.documentElement.classList.remove("no_js");
+
 //HANDLE SIDEBAR OPENING
 
 const sidebar = document.querySelector(".sidebar");
@@ -16,3 +18,66 @@ function openNav() {
         isOpen = false;
     }
 }
+
+//HANDLE PHOTO GRID LOADING ANIMATION + PAGE TRANSITIONS
+
+document.addEventListener("DOMContentLoaded", () => {
+    const photosGridContainer = document.querySelector('.photos_grid_container');
+        
+    if (photosGridContainer) {
+        const images = photosGridContainer.querySelectorAll('img');
+
+        let imagesLoaded = 0;
+        const totalImages = images.length;
+
+        if (totalImages === 0) {
+            photosGridContainer.classList.add('loaded');
+            return;
+        }
+
+        images.forEach((img) => {
+            if (img.complete) {
+                increment();
+            }
+            else {
+                img.addEventListener("load", increment);
+                img.addEventListener("error", increment);
+            }
+        });
+
+        function increment() {
+            imagesLoaded++;
+            if (imagesLoaded === totalImages) {
+                photosGridContainer.classList.add('loaded');
+            }
+        }
+    }
+
+    const page = document.getElementById("page");
+    console.log("page: ", page);
+
+    if (page) {
+        page.classList.add("loaded");
+
+        document.querySelectorAll("a[href]").forEach(link => {
+            const url = link.getAttribute("href");
+
+            if (
+                link.target === "_blank" ||
+                url.startsWith("#") ||
+                url.startsWith("http")
+            ) return;
+
+            link.addEventListener("click", e => {
+                e.preventDefault();
+                page.classList.remove("loaded");
+                page.classList.add("fade-out");
+
+                setTimeout(() => {
+                    window.location.href = url;
+                }, 500);
+            });
+        });
+    }
+});
+
